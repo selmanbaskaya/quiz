7da0063debaa02b452a27b83c77834b19ec0cd99
Selman <selmanbaskaya1@gmail.com>
2021-01-10 02:31:43 +0300

    seed structure split & quiz added with faker lib

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::insert([
+            'name'=>'Selman Başkaya',
+            'email'=>'selmanbaskaya1@gmail.com',
+            'email_verified_at' => now(),
+            'type'=>'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\User::factory(10)->create();
+    }
+}
